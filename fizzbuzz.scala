efbbe4b11a0cc0d9ab91a5d85ca161787d4680fa
h0tcat <zer0busamx@yahoo.co.jp>
2020-04-18 15:56:28 +0900

    update commit

--- fizzbuzz.scala
+++ fizzbuzz.scala
@@ -1,11 +1,10 @@
-
 object fizzbuzz extends App{
-    var min,max :Int=_;
-    min=1;
-    max=99;
-
-    for(loop <- min to max){
-        val fizzbuzz_count=if(loop % 3== 0 && loop % 5==0) "FizzBuzz" else if(loop % 3==0) "Fizz" else if(loop % 5==0) "Buzz" else loop;
-        println(fizzbuzz_count);
+    def judge(n:Int)={
+        if(n % 15 == 0) "FizzBuzz"
+        else if(n % 3 == 0) "Fizz"
+        else if(n % 5 == 0) "Buzz"
+        else n
     }
+    lazy val value = 1 to 30
+    value.foreach{n:Int=>println(judge(n))}
 }
